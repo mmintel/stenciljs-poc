de93b66edc004d75fb59d55b0a7cb8d184671d3e
Marc Mintel <marc@mintel.me>
2020-07-06 13:19:56 +0700

    add netlify

--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -1,10 +1,6 @@
-declare global {
-  import { LogLevel } from '../src/utils/logger';
-
-  namespace NodeJS {
-    interface ProcessEnv {
-      NODE_ENV: 'development' | 'production';
-      LOG_LEVEL: Loglevel;
-    }
-  }
+export interface ProcessEnv {
+  NODE_ENV: 'development' | 'production';
+  LOG_LEVEL: 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+  CONTENTFUL_SPACE_ID: string;
+  CONTENTFUL_ACCESS_TOKEN: string;
 }
